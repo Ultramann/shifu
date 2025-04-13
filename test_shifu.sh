8f80e9243d4abfa72309d567dfd2a90d9b3ca09a
Cary Goltermann <cary.goltermann@gmail.com>
2025-04-13 19:57:35 -0400

    Clean test code format

--- test_shifu.sh
+++ test_shifu.sh
@@ -2,7 +2,7 @@
 
 shifu_test_function_declaration='^(test_.*) ?\(\) {'
 shifu_read_test_functions() {
-  local test_script_path=$1
+  local test_script_path="$1"
   test_functions=$(
     cat "$test_script_path" | \
     # -r: extended regex, -n: don't echo lines to stdout
@@ -13,20 +13,20 @@ shifu_read_test_functions() {
 
 shifu_report_success() {
   if [ "$shifu_verbose_tests" = true ]; then
-    printf "$shifu_green%-10s$shifu_reset%s\n" success $1
+    printf "$shifu_green%-7s$shifu_reset%s\n" pass $1
   fi
 }
 
 shifu_report_failure() {
-  printf "$shifu_red%-10s$shifu_reset%s\n" fail $1
+  printf "$shifu_red%-7s$shifu_reset%s\n" fail $1
 }
 
 shifu_report_context() {
-  local header=$1; shift
-  printf "$shifu_grey%10s%s$shifu_reset\n" "" "$header"
-  local arg
-  for arg in "$@"; do
-    printf "$shifu_grey%12s%s$shifu_reset\n" "" "$arg"
+  local header="$1"; shift
+  printf "$shifu_grey%7s%s$shifu_reset\n" "" "$header"
+  local argument
+  for argument in "$@"; do
+    printf "$shifu_grey%10s%s$shifu_reset\n" "" "$argument"
   done
 }
 
@@ -138,12 +138,13 @@ test_shifu_array_filter_prefix_empty_prefix() {
   shifu_assert_strings_equal "$actual" "$expected"
 }
 
-test_shifu_infer_function_to_call() {
+test_shifu_determine_function_to_call() {
   local script_functions="$(shifu_iterate "test_ test_sub_ test_sub_func")"
 
   local expected_function_to_call="test_sub_func"
 
-  shifu_infer_function_to_call test sub func arg1 arg2; local status=$?
+  local function_to_call
+  shifu_determine_function_to_call test sub func arg1 arg2; local status=$?
 
   shifu_assert_zero $status
   shifu_assert_strings_equal "$function_to_call" "$expected_function_to_call"
@@ -159,21 +160,22 @@ test_shifu_infer_function_and_arguments_only_subcommand() {
 
   local expected_function_to_call=""
 
-  shifu_infer_function_to_call test sub arg1 arg2; local status=$?
+  local function_to_call
+  shifu_determine_function_to_call test sub arg1 arg2; local status=$?
 
   shifu_assert_non_zero $status
   shifu_assert_strings_equal "$function_to_call" "$expected_function_to_call"
 }
 
 shifu_run_test() {
-  local test_function=$1
+  local test_function="$1"
   local errors=0
   "$test_function" 2> /dev/null
   return $errors
 }
 
 shifu_run_test_and_report() {
-  local test_function=$1
+  local test_function="$1"
   message=$(shifu_run_test "$test_function")
   local test_result=$?
   if [ "$test_result" -eq 0 ]; then
@@ -182,29 +184,33 @@ shifu_run_test_and_report() {
     shifu_report_failure "$test_function"
     echo "$message"
   fi
-  return ${test_result}
+  return $test_result
 }
 
 shifu_run_test_suite() {
-  [ $# -gt 0 ] && [ $1 = "-v" ] && shifu_verbose_tests=true
+  [ $# -gt 0 ] && [ "$1" = "-v" ] && shifu_verbose_tests=true
+
+  local test_functions
   shifu_read_test_functions $(shifu_caller_script)
-  local failures=0
-  local total=0
+
+  local n_failed=0
+  local n_tests=0
   for test_function in $(shifu_iterate "$test_functions"); do
     shifu_run_test_and_report "$test_function"
-    failures=$(($failures + $?))
-    total=$(($total + 1))
+    n_failed=$(($n_failed + $?))
+    n_tests=$(($n_tests + 1))
   done
-  local successs=$(($total - $failures))
-  local successful=$(echo "scale=2; $successs * 100 / $total" | bc)
-  if [ $failures -eq 0 ]; then
+
+  local n_passed=$(($n_tests - $n_failed))
+  local percent_passed=$(echo "scale=2; $n_passed * 100 / $n_tests" | bc)
+  if [ $n_failed -eq 0 ]; then
     local color="$shifu_green"
   else
     local color="$shifu_red"
   fi
-  local test_report="Tests $successful% successful"
-  echo "============== $color $test_report $shifu_reset =============="
+  local test_report="$percent_passed% tests passed"
+  echo "================ $color $test_report $shifu_reset ================"
   exit $failures
 }
 
-shifu_run_test_suite $@
+shifu_run_test_suite "$@"
